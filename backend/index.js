bd440ca533f4b5e2d2ff7abe9d5c3df502fd2c64
Yaswanth14 <yaswanth14333@gmail.com>
2024-01-22 17:41:08 +0530

    zod validation for post and put

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+const { createTodo, updateTodo } = require("./types");
 
 app.use(express.json());
 
 app.post("/todo", (req, res)=>{
-
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            message: "You send a wrong input"
+        })
+        return;
+    }
+    // Put in mongoDB
 })
 
 app.get("todos", (req, res)=>{
@@ -12,7 +21,15 @@ app.get("todos", (req, res)=>{
 })
 
 app.put("/completed", (req, res)=>{
-
+    const updatePayload = req.body;
+    const parsePayload = updateTodo.safeParse(updatePayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            message: "You send a wrong input"
+        })
+        return;
+    }
+    // Put in mongoDB
 })
 
 const PORT = 3000;
